d9cb5411e926c90e4cfdf412165cf9bab6e3c384
anand2312 <anandkrishna2312@gmail.com>
2021-11-06 10:21:44 +0530

    chore: switch column to multidict (part 1)

--- pgrest/column.py
+++ pgrest/column.py
@@ -2,56 +2,19 @@ from __future__ import annotations
 
 from typing import Any, Union
 
-from pgrest.utils import sanitize_param
+from multidict import MultiDict
 
-ParamDict = dict[str, "ParamDict"] | dict[str, str]
+from pgrest.utils import sanitize_param
 
 
 class Condition:
-    def __init__(self, params: ParamDict) -> None:
-        self.params = params  # dict of format column: operator.value
-
-    """ def __and__(self, other: Condition) -> Condition:
-        new_value = self._parse_params(other)
-        self.params = {"and": new_value}
-        return self
-
-    def __or__(self, other: Condition) -> Condition:
-        new_value = self._parse_params(other)
-        self.params = {"or": new_value}
-        return self
-
-    def _parse_params(self, other: Condition) -> str:
-        # parse the params to form new params
-        new_value = ""
-        if len(other.params) == 1:  # only one key, possibly AND/OR
-            top_key, top_value = list(other.params.items())[0]
-            if top_key in ["and", "or"]:
-                # no longer a top-level param
-                # their value has to be wrapped into parentheses
-                new_value += f",{top_key}"
-                new_value += f"({top_value})"
-            else: # normal condition
-                new_value += f",{top_key}.{top_value}"
-        else:
-            new_value += "," + ",".join([".".join(item) for item in other.params.items()])
-
-        if len(self.params) == 1:
-            top_key, top_value = list(self.params.items())[0]
-            if top_key in ["and", "or"]:
-                # no longer a top-level param
-                # their value has to be wrapped into parentheses
-                new_value += f",{top_key}"
-                new_value += f"({top_value})"
-            else: # normal condition
-                new_value += f",{top_key}.{top_value}"
-        else:
-            new_value += "," + ",".join([".".join(item) for item in self.params.items()])
-
-        return new_value"""
+    def __init__(self, params: Union[dict, MultiDict]) -> None:
+        self.params = MultiDict(params)  # dict of format column: operator.value
 
     def __and__(self, other: Condition) -> Condition:
-        new_params = {"and": {**self.params, **other.params}}
+        new_params = {
+            "and": {**self.params, **other.params}
+        }  # TODO: bugs because of possible duplicate and/or keys at the same level of nesting
         return Condition(new_params)
 
     def __or__(self, other: Condition) -> Condition:
@@ -60,7 +23,17 @@ class Condition:
 
     def flatten_params(self) -> dict[str, str]:
         """parse self.params into a valid query string"""
-        raise NotImplementedError
+        check = lambda i: isinstance(i, dict)
+
+        # is the top-level key an "and"/"or"?
+        # if it is, there are multiple conditions, nested indefinitely deep
+        # travel down, merging them all into a single string
+        # if not, there's only a single condition, return directly.
+        params = self.params.copy()
+        done = {}
+
+        while True:
+            top_key, top_value = list(params.items())[0]
 
     def __repr__(self) -> str:
         return f"<Condition {' '.join([f'{item[0]}={item[1]}' for item in self.params.items()])}>"
